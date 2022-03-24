db0ac0c204c797900870db6305299d900e3af683
JavenW <513203628@qq.com>
2022-03-24 06:23:14 -0400

    add contents

---
+++ index-photos-copy/lambda_function.py
@@ -0,0 +1,94 @@
+import json
+import boto3
+import urllib.parse
+import datetime
+import requests
+
+def detect_rekog_labels(photo, bucket):
+
+    client=boto3.client('rekognition')
+
+    response = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':photo}},
+        MaxLabels=10, MinConfidence=60)
+
+    print('Detected labels for ' + photo) 
+    labels = []
+    for label in response['Labels']:
+        labels.append(label['Name'])
+    return labels
+
+def retrive_custom_labels(photo, bucket):
+
+    client = boto3.client('s3')
+    headObjects = client.head_object(
+        Bucket=bucket,
+        Key=photo,
+    )
+    print(headObjects)
+    time = headObjects["LastModified"].isoformat()
+    if "customlabels" not in headObjects["Metadata"]:
+        return time, []
+    customLabels = str(headObjects["Metadata"]["customlabels"]).split(",")
+    
+    return time, customLabels
+    
+
+def store_opensearch(picName, json_data):
+    
+    headers = {
+        # Already added when you pass json= but not when you pass data=
+        # 'Content-Type': 'application/json',
+    }
+    
+    response = requests.put("https://search-cchw2photosopensearch-sy3nt7l7m7an5goaumpbfl6pqe.us-east-1.es.amazonaws.com/photos/_doc/"+str(picName), headers=headers, json=json_data, auth=('master', 'Wjw,513203628'))
+    
+    print(response)
+    
+    
+
+def lambda_handler(event, context):
+    # TODO implement
+    
+    print(event)
+    
+    s3 = event["Records"][0]["s3"]
+    bucket = s3["bucket"]["name"]
+    urlName = s3["object"]["key"]
+    picName = urllib.parse.unquote_plus(urlName)
+    print("picName ", picName)
+    
+    rekogLabels=detect_rekog_labels(picName, bucket)
+    rekogLabels = [x.lower() for x in rekogLabels]
+    print("Labels detected: " + str(rekogLabels))
+    
+    # rekogLabels = ["rek1", "rek2"]
+    
+    time, customLabels = retrive_custom_labels(picName, bucket)
+    
+    # time = "2011-11-04T00:05:23"
+    # customLabels = ["cus1","cus2"]
+    
+    customLabels = [x.lower() for x in customLabels]
+    
+    print("customLabels: ", customLabels)
+    
+    totalLabels  = list(set(rekogLabels + customLabels))
+    
+    print("totalLabels: ", totalLabels)
+    
+    ret_dict = {
+        "objectKey": picName,
+        "bucket": bucket,
+        "createdTimestamp": time,
+        "labels": totalLabels,
+        "url": "https://cchw2photostorage.s3.amazonaws.com/"+urlName
+    }
+    
+    print("dict: ", str(ret_dict))
+    
+    store_opensearch(picName, ret_dict)
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Successful actions from Lambda index-photos!')
+    }
---
+++ search-photos-copy/lambda_function.py
@@ -0,0 +1,116 @@
+import json
+import boto3
+import requests
+
+def lex2_disambiguate(send_message):
+     # Define the client to interact with Lex
+    client = boto3.client('lexv2-runtime', region_name='us-east-1')
+    
+    response = client.recognize_text(
+        botId='J1KQWOMJRX',
+        botAliasId='TSTALIASID',
+        localeId='en_US',
+        sessionId="test_session",
+        text=send_message)
+        
+    print("response: ", response)
+    
+    content = response['messages'][0]['content']
+    
+    if content == "Please show me some keywords you want to see." or content == "i am still learning what you said', 'please come back later.":
+        return []
+    
+    msg_from_lex = content.split(", ")
+    keywork_list = [x.strip().lower() for x in msg_from_lex]
+    
+    return keywork_list
+    
+
+def search_OpenSearch(keywords):
+    
+    host = 'https://search-cchw2photosopensearch-sy3nt7l7m7an5goaumpbfl6pqe.us-east-1.es.amazonaws.com' # The OpenSearch domain endpoint with https://
+    index = "photos"
+    url = host + '/' + index + '/_search'
+    
+    # Put the user query into the query DSL for more accurate search results.
+    # Note that certain fields are boosted (^).
+    query = {}
+    if len(keywords) == 1:
+        query = {
+            "size": 100,
+            "query": {
+                "term": {
+                    "labels": keywords[0],
+                }
+            }
+        }
+    else:
+        query = {
+          "query": {
+            "bool" : {
+              "should": [
+                { "term": { "labels": keywords[0] }},
+                { "term": { "labels": keywords[1] }},
+              ]
+            }
+          }
+        }
+
+
+    # Elasticsearch 6.x requires an explicit Content-Type header
+    headers = { "Content-Type": "application/json" }
+
+    # Make the signed HTTP request
+    r = requests.get(url, auth=('master', 'Wjw,513203628'), headers=headers, data=json.dumps(query))
+
+    # Create the response and add some extra content to support CORS
+    response = {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": '*'
+        },
+        "isBase64Encoded": False
+    }
+
+    # Add the search results to the response
+    lst = []
+    print(r.text)
+    hits = json.loads(r.text)["hits"]["hits"]
+    for hit in hits:
+        dic = hit["_source"]["url"]
+        lst.append(dic)
+    return lst
+    
+
+def lambda_handler(event, context):
+    # TODO implement
+    print("event", event)
+    print("context", context)
+    # message = "I would like to see Motorcycle and pen"
+    message = event["queryStringParameters"]["q"]
+    print("message: ", message)
+    keywork_list = lex2_disambiguate(message)
+    
+    print("keywords: ", str(keywork_list))
+    
+    if keywork_list:
+        lst = search_OpenSearch(keywork_list)
+        # lst = search_OpenSearch(["rek1","cus11","no"])
+
+        resp = {
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin": '*'
+            },
+            "isBase64Encoded": False,
+            "body": json.dumps(lst)
+        }
+    else:
+        resp = {
+            'statusCode': 200,
+            # 'body': "json.dumps({"results": []})"
+            'body': json.dumps([])
+        }
+    
+    
+    return resp
